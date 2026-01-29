bc431ecd448bce47e88cc1e082da5f091403dbb6
0xShadowHex <nipan.zuzuzu587@gmail.com>
2026-01-29 04:49:47 -0800

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-ocument.getElementById('meinButton').addEventListener('click', function() {
-    const buttonText = this.innerText; // Text des Buttons abrufen
-    document.getElementById('textDisplay').innerText = buttonText; // Text anzeigen
